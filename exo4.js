c06b48e0566692b90aee0c1fe8857b4e49fc3982
damokless <damokless@protonmail.com>
2020-09-16 23:51:43 +0200

    suppression de l'exo 4

--- exo4.js
+++
@@ -1,9 +0,0 @@
-function capitalize(str= ''){
-    for(let i=0; i< str.length; i++){
-        const code = str.charCodeAt(i)
-        console.log(code)
-        
-    }
-}
-console.log(capitalize("one Code"))
-console.log(capitalize("One CODE"))
