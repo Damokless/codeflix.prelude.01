571f94b5bb7839502f98d527106b5c6b4e2b2af3
damokless <damokless@protonmail.com>
2020-09-16 23:05:28 +0200

    ajout de l'exercice 1 sans fonction prédefini par js

---
+++ exo4.js
@@ -0,0 +1,9 @@
+function capitalize(str= ''){
+    for(let i=0; i< str.length; i++){
+        const code = str.charCodeAt(i)
+        console.log(code)
+        
+    }
+}
+console.log(capitalize("one Code"))
+console.log(capitalize("One CODE"))
